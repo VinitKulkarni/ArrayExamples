51a78c8b4ece8a3d858762dec2f7c33dfbe69ec0
vinit <vinitkkulkarni@gmail.com>
2023-04-07 18:53:48 +0530

    Refactore: only duplicate element will be printed

--- ArrayExample8.java
+++ ArrayExample8.java
@@ -5,14 +5,29 @@ package com.bridgelabz.ArrayExamples;
 public class ArrayExample8 {
     public static void main(String[] args) {
         int arrayData[] = {100,10,1,2,3,100,10,4,4,100};
-
+        int distinctElements [] = new int[arrayData.length];
 
         for(int i=0; i<arrayData.length; i++){
-            for(int j=i+1; j<arrayData.length; j++){
-                if (arrayData[i] == arrayData[j]){
-                    System.out.println(arrayData[i]);
+            boolean flag = false;
+            if(i == 0){
+                distinctElements[i] = arrayData[i];
+            }
+            int temp = arrayData[i];
+            for(int j=0; j<distinctElements.length; j++){
+                if (temp != distinctElements[j]){
+                    flag = true;
+                }else{
+                    flag = false;
+                    break;
                 }
             }
+            if(flag == true){
+                distinctElements[i] = temp;
+            }
+        }
+        for (int value:distinctElements) {
+            if(value > 0)
+                System.out.println(value);
         }
     }
 }
